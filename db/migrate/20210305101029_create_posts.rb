36d4b4dbae09948ac79cef2ca288f6405cb9df99
Kogawa <utyuu1007@yahoo.co.jp>
2021-04-02 10:16:33 +0000

    コメント

--- db/migrate/20210305101029_create_posts.rb
+++ db/migrate/20210305101029_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :name
+      t.string :names
+      t.string :comment
       t.integer :newthread_id
 
       t.timestamps
