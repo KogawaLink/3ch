c31c9ca86a1b313c621e7d494caf22be463b3663
Kogawa <utyuu1007@yahoo.co.jp>
2021-04-07 06:45:56 +0000

    コメント追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,18 @@
 class PostsController < ApplicationController
  
  def index
-  @newthread = Newthread.find(params[:id])
+  @newthread = Newthread.includes(:posts).find(params[:id])
   @post = @newthread.posts.new
  end
  
  def create
-  @newthread = Newthread.find(params[:newthread_id])
-  @post = @newthread.posts.new(post_params)
-  
-  if @post.save
-   redirect_to posts_path(@newthread)
-  else
-   render "newthreads/new"
-  end
+   @newthread = Newthread.find(params[:newthread_id])
+   @post = @newthread.posts.new(post_params)
+   if @post.save
+     redirect_to posts_path(@newthread)
+   else
+     render "newthreads/new"
+   end
  end
 
  private
