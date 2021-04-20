0ef7aaf0216483e9a9ab6bb25eb3c939a546115c
Kogawa <utyuu1007@yahoo.co.jp>
2021-04-20 05:59:06 +0000

    スレッド・コメント同時削除

--- app/controllers/newthreads_controller.rb
+++ app/controllers/newthreads_controller.rb
@@ -20,13 +20,14 @@ class NewthreadsController < ApplicationController
   
   def delete
     @thread = Newthread.find(params[:id])
-    @post = Post.where(newthread_id: @thread)
+    @post = Post.where(newthread_id: [:id])
   end
   
   def destroy
     @thread = Newthread.find_by(id: params[:id])
     @post = Post.find_by(id: params[:id])
     if  @thread.pw.eql?(params[:newthread][:again_password]) && @thread.destroy!
+      @post.destroy!
       redirect_to root_path, success: '削除しました。'
     else
       flash.now[:danger] = 'パスワードが違います'
