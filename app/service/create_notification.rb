b0845a13e8fa1adfd0858a8730a109cb99090e3f
Kogawa <utyuu1007@yahoo.co.jp>
2021-05-13 16:28:39 +0000

    onesignla

--- app/service/create_notification.rb
+++ app/service/create_notification.rb
@@ -22,15 +22,15 @@ class CreateNotification
 
   def headers
     {
-      'Authorization' => 'Basic ZDI2ZGFlYzAtMWQ0OC00MjYyLTk0ZjktNmRjOTgyZWI2YTk4',
+      'Authorization' => "Basic YTg3ZTUyMDQtN2JlZC00NTJlLThmOWMtNjVhMWNlY2IwNTU5",
       'Content-Type'  => 'application/json'
     }
   end
 
   def body
     {
-      'app_id' => 'your-app-id',
-      'url'    => 'localhost:3000',
+      'app_id' => '32ecc738-095a-49b2-92c3-5da9652d834f',
+      'url'    => 'https://95d2aee039d34d0685c446f0862e07fa.vfs.cloud9.us-east-2.amazonaws.com',
       'data'   => { 'type': type },
       'included_segments' => ['All'],
       'contents' => contents
