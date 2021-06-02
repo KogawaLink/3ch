8ae48bbce02b80c576a4f8204a9b339c5b82ba0a
Kogawa <utyuu1007@yahoo.co.jp>
2021-06-02 14:46:29 +0000

    環境変数設定

--- app/service/create_notification.rb
+++ app/service/create_notification.rb
@@ -22,14 +22,14 @@ class CreateNotification
 
   def headers
     {
-      'Authorization' => "Basic YTg3ZTUyMDQtN2JlZC00NTJlLThmOWMtNjVhMWNlY2IwNTU5",
+      'Authorization' => "Basic #{ENV['ONESIGNAL_AUTHKEY']}",
       'Content-Type'  => 'application/json'
     }
   end
 
   def body
     {
-      'app_id' => '32ecc738-095a-49b2-92c3-5da9652d834f',
+      'app_id' => ENV['ONESIGNAL_APPID'],
       'url'    => 'https://95d2aee039d34d0685c446f0862e07fa.vfs.cloud9.us-east-2.amazonaws.com',
       'data'   => { 'type': type },
       'included_segments' => ['All'],
