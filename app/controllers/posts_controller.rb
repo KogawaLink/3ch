4599528ca6a44d6d7e90acb58d7601f7c383146d
Kogawa <utyuu1007@yahoo.co.jp>
2021-06-17 12:20:46 +0000

    ページネーション修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,9 @@ class PostsController < ApplicationController
  def index
   @newthread = Newthread.includes(:posts).find(params[:id])
   @newthread_posts = @newthread.posts.order("created_at desc").page(params[:page]).per(5)
+  page = params[:page].nil? ? 1 : params[:page].to_i  #<%  ページが空だった場合１を代入し、空じゃない場合はページ数を数値として取得 %>
+  @start = (page - 1) * 5  #<%  startを定義しページ数に対するコメント番号を計算 %>
+  @postcount = @newthread.posts.count
   @post = @newthread.posts.new
  end
  
@@ -10,8 +13,8 @@ class PostsController < ApplicationController
   @post = @newthread.posts.new(post_params)
   @post.ip_address = request.remote_ip
   @postname = @post.postname
-  if @postname.nil?
-   @postname = @noname
+  if @postname.blank?
+   @post.postname = "名無しさん"
   end
   if @post.save
        CreateNotification.call(
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
  belongs_to :newthread, optional: true
  mount_uploader :image, ImageUploader
  
- @noname = "名無しさん"
+
  validates :comment, presence: true
 end
