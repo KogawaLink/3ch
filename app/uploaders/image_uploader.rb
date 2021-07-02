63ae7849850634f8c0875877f20a43cd6a811ef9
Kogawa <utyuu1007@yahoo.co.jp>
2021-07-02 22:33:09 +0000

    投稿画像リサイズ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,10 +2,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
+  process resize_to_limit: [750, 750]
   # Choose what kind of storage to use for this uploader:
   storage :fog
-  # storage :fog
+  # storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -33,9 +33,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   process resize_to_fit: [50, 50]
   # end
 
-  version :medium do
-    process resize_to_fill: [750, 750]
-  end
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
